17e9bc3f63b2af59d972d135023cecb4355748b0
Nich Galzin <nichgalzin@gmail.com>
2024-09-23 19:14:56 +0100

    fix: renames background script file in line with plasmo documentation
    Relates #21

---
+++ src/background/index.ts
@@ -0,0 +1,12 @@
+export {}
+
+if (process.env.NODE_ENV === "development") {
+  console.log("This is a development build")
+}
+
+if (process.env.NODE_ENV === "production") {
+  console.log("This is a production build")
+}
+
+console.log("SITE_URL:", process.env.PLASMO_PUBLIC_SITE_URL)
+console.log("Hi from Background!")
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react"
+
+import { sendToBackground } from "@plasmohq/messaging"
+
 // utils
 import useStore from "~store/store"
 
 // styles
 import "./components/normalize.css"
 
+//types
+import type { ProjectResponse } from "~types/projectTypes"
+
 // components
 import Layout from "./components/Layout/Layout"
 
 function IndexPopup() {
-  const currentProject = useStore.use.currentProject()
+  const currentProjectFromStore = useStore.use.currentProject()
+  const [currentProject, setCurrentProject] =
+    useState<ProjectResponse>()
+
+  useEffect(() => {
+    const updateCurrentProject = async () => {
+      const response = await sendToBackground({
+        name: "fetchCurrentProject"
+      })
+      console.log("Hi from useEffect")
+      console.log({ response })
+      setCurrentProject(response)
+    }
+    updateCurrentProject()
+  }, [])
 
   return (
     <Layout>
@@ -20,7 +41,15 @@ function IndexPopup() {
           justifyContent: "center"
         }}
       >
-        <h2>{currentProject}</h2>
+        {currentProject && (
+          <h2>
+            {
+              currentProject.data.attributes.project.data.attributes
+                .title
+            }
+          </h2>
+        )}
+        <h3>{currentProjectFromStore}</h3>
       </div>
     </Layout>
   )
