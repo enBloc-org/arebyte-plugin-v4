87e074006b7fdb71fbd35c9bfe6e6fd8da77abaf
Nich Galzin <nichgalzin@gmail.com>
2024-09-30 18:59:28 +0100

    refactor: trigger now fetches pop information for events and then triggers popups

--- src/background/messages/triggerPopup.ts
+++ src/background/messages/triggerPopup.ts
@@ -1,9 +1,26 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
+// import { Storage } from "@plasmohq/storage"
+
+import { eventPopupQueryString } from "~queries/eventPopupsQuery"
+import type { EventResponse } from "~types/eventTypes"
 import backgroundPopupCreate from "~utils/backgroundPopCreate"
+import { fetchStrapiContent } from "~utils/fetchStrapiContent"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  backgroundPopupCreate(req.body.images)
+  // const storage = new Storage()
+  console.log(eventPopupQueryString)
+
+  const { data } = await fetchStrapiContent<EventResponse>(
+    `api/events/${req.body.id}?${eventPopupQueryString}`
+  )
+
+  // await storage.set("popups", data.pop_ups)
+
+  console.log("====================================")
+  console.log(data)
+  console.log("====================================")
+  backgroundPopupCreate(data.pop_ups)
   res.send("success!")
 }
 
