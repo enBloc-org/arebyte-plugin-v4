f526e188fceac986cd207ea755fbd13d9fcaaff7
Nich Galzin <nichgalzin@gmail.com>
2024-09-30 18:58:31 +0100

    feat: evaluates data for size and positioning and creates appropiate popup windows
    Relates #27

--- src/utils/backgroundPopCreate.ts
+++ src/utils/backgroundPopCreate.ts
@@ -1,19 +1,75 @@
-const backgroundPopupCreate = event => {
-  event.forEach((popup, index) => {
+import Browser from "webextension-polyfill"
+
+const backgroundPopupCreate = async popups => {
+  const currentWindow = await Browser.windows.getCurrent()
+  const screenHeight = currentWindow.height
+  const screenWidth = currentWindow.width
+
+  popups.forEach((popup, index) => {
+    let top: number, left: number
+
+    const formatKey = popup.popup_size.toLowerCase().trim()
+    const format = popup.popup_content[0].media.formats[formatKey]
+
+    const height =
+      format?.height ?? popup.popup_content[0].media.height
+
+    const width = format?.width ?? popup.popup_content[0].media.width
+
+    switch (true) {
+      case /^\s*top\s*right\s*$/i.test(popup.position):
+        top = 0
+        left = screenWidth - width
+        break
+      case /^\s*top\s*center\s*$/i.test(popup.position):
+        top = 0
+        left = (screenWidth - width) / 2
+        break
+      case /^\s*top\s*left\s*$/i.test(popup.position):
+        top = 0
+        left = 0
+        break
+      case /^\s*center\s*right\s*$/i.test(popup.position):
+        top = (screenHeight - height) / 2
+        left = screenWidth - width
+        break
+      case /^\s*center\s*$/i.test(popup.position):
+        top = (screenHeight - height) / 2
+        left = (screenWidth - width) / 2
+        break
+      case /^\s*center\s*left\s*$/i.test(popup.position):
+        top = (screenHeight - height) / 2
+        left = 0
+        break
+      case /^\s*bottom\s*right\s*$/i.test(popup.position):
+        top = screenHeight - height
+        left = screenWidth - width
+        break
+      case /^\s*bottom\s*center\s*$/i.test(popup.position):
+        top = screenHeight - height
+        left = (screenWidth - width) / 2
+        break
+      case /^\s*bottom\s*left\s*$/i.test(popup.position):
+        top = screenHeight - height
+        left = 0
+        break
+      default:
+        console.log("Default position")
+
+        top = (screenHeight - height) / 2
+        left = (screenWidth - width) / 2
+        break
+    }
+
     setTimeout(() => {
-      chrome.windows.create(
-        {
-          url: "/tabs/newTab.html",
-          type: "popup",
-          width: popup.width,
-          height: popup.height,
-          top: popup.top,
-          left: popup.left
-        },
-        newWindow => {
-          console.log(newWindow.id)
-        }
-      )
+      Browser.windows.create({
+        url: "/tabs/newTab.html",
+        type: "popup",
+        width: width,
+        height: height,
+        top: Math.floor(top),
+        left: Math.floor(left)
+      })
     }, index * 1000)
   })
 }
