839de19d01bcd01d77e58feee7bac9a553884d93
Nich Galzin <nichgalzin@gmail.com>
2024-10-01 10:28:43 +0100

    refactor: removes storage and console log
    Relates #27

--- src/background/messages/triggerPopup.ts
+++ src/background/messages/triggerPopup.ts
@@ -1,25 +1,15 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-// import { Storage } from "@plasmohq/storage"
-
 import { eventPopupQueryString } from "~queries/eventPopupsQuery"
 import type { EventResponse } from "~types/eventTypes"
 import backgroundPopupCreate from "~utils/backgroundPopCreate"
 import { fetchStrapiContent } from "~utils/fetchStrapiContent"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  // const storage = new Storage()
-  console.log(eventPopupQueryString)
-
   const { data } = await fetchStrapiContent<EventResponse>(
     `api/events/${req.body.id}?${eventPopupQueryString}`
   )
 
-  // await storage.set("popups", data.pop_ups)
-
-  console.log("====================================")
-  console.log(data)
-  console.log("====================================")
   backgroundPopupCreate(data.pop_ups)
   res.send("success!")
 }
