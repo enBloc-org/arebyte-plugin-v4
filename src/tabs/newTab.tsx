3aca80d244088e943a29345fdca5ea102666a53d
Nich Galzin <nichgalzin@gmail.com>
2024-10-01 10:30:23 +0100

    feat: basic popup window to render images
    Relates #27

--- src/tabs/newTab.tsx
+++ src/tabs/newTab.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react"
-
 const NewTab = () => {
-  const [count, setCount] = useState(0)
+  const params = new URLSearchParams(window.location.search)
+  const width = params.get("width")
+  const height = params.get("height")
+  const url = "http://localhost:1337" + params.get("url")
+
   return (
-    <button onClick={() => setCount(prevState => prevState + 1)}>
-      {count}
-    </button>
+    <div>
+      <img
+        src={url}
+        alt="an image"
+        style={{ width: Number(width), height: Number(height) }}
+      />
+    </div>
   )
 }
 
