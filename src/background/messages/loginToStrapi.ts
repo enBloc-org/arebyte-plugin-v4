236733b99faf3f67f508367dfb4b11fd7e2f12df
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-03 11:14:51 +0100

    style: clean comments
    relates #33

--- src/background/messages/loginToStrapi.ts
+++ src/background/messages/loginToStrapi.ts
@@ -4,19 +4,6 @@ import type { AuthResponse } from "~types/userTypes"
 import { fetchStrapiContent } from "~utils/fetchStrapiContent"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  // const response = await fetch(
-  //   `${process.env.PLASMO_PUBLIC_API_URL}/api/auth/local`,
-  //   {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     },
-  //     body: req.body
-  //   }
-  // )
-  // const data = await response.json()
-  // console.dir(data)
-
   const data = await fetchStrapiContent<AuthResponse>(
     `api/auth/local`,
     "POST",
