d35fde523ee7083acbce846c655efdbe9254e531
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-07 12:10:40 +0100

    refactor: return new UserSession object
    relates #48

--- src/background/messages/loginToStrapi.ts
+++ src/background/messages/loginToStrapi.ts
@@ -1,16 +1,37 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-import type { AuthResponse } from "~types/userTypes"
+import type {
+  AuthResponse,
+  User,
+  UserSession
+} from "~types/userTypes"
 import { fetchStrapiContent } from "~utils/fetchStrapiContent"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  const data = await fetchStrapiContent<AuthResponse>(
+  const authData = await fetchStrapiContent<AuthResponse>(
     `api/auth/local`,
     "POST",
     undefined,
     req.body
   )
-  res.send(data)
+
+  const userData = await fetchStrapiContent<User>(
+    `api/audience-members/${authData.user.id}`,
+    "GET",
+    authData.jwt
+  )
+
+  const response: UserSession = {
+    jwt: authData.jwt,
+    id: authData.user.id,
+    event_time: userData.audience_member.event_time,
+    project_id: userData.audience_member.project_id,
+    current_index: userData.audience_member.current_index,
+    is_paused: userData.audience_member.is_paused,
+    is_quiet: userData.audience_member.is_quiet
+  }
+
+  res.send(response)
 }
 
 export default handler
