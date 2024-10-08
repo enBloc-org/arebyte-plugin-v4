9ae5926c0b5a6e93a06054147fdfdcc4445957dd
Nich Galzin <nichgalzin@gmail.com>
2024-10-08 15:38:45 +0100

    refactor: fixes type error with assertion of correct data type
    Relates #36

--- src/components/page-components/CurrentProjectPage/CurrentProjectPage.tsx
+++ src/components/page-components/CurrentProjectPage/CurrentProjectPage.tsx
@@ -6,9 +6,11 @@ import BackButton from "~components/BackButton/BackButton"
 import CuratorDetails from "~components/CuratorDetails/CuratorDetails"
 import Footer from "~components/Footer/Footer"
 import ProjectDetails from "~components/ProjectDetails/ProjectDetails"
+import type { ProjectData } from "~types/projectTypes"
 
 const CurrentProjectPage = () => {
   const active_project = useStore.use.active_project()
+  const project = active_project.data as ProjectData
 
   return (
     <>
@@ -17,18 +19,16 @@ const CurrentProjectPage = () => {
         <img
           src={
             process.env.PLASMO_PUBLIC_API_URL +
-            active_project.data.project.cover_image.formats.small.url
+            project.cover_image.formats.small.url
           }
           alt={
-            active_project.data.project.cover_image.alternativeText ||
+            project.cover_image.alternativeText ||
             "Project image thumbnail"
           }
         />
         <div className="grid project-details__container">
-          <ProjectDetails project={active_project.data.project} />
-          <CuratorDetails
-            curator={active_project.data.project.content_creator}
-          />
+          <ProjectDetails project={project} />
+          <CuratorDetails curator={project.content_creator} />
         </div>
         <Footer />
       </main>
