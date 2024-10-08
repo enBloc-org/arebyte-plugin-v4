274d121ab9bc089104a0f1a84136547f421149f5
Nich Galzin <nichgalzin@gmail.com>
2024-10-08 20:16:44 +0100

    style: adds styling and html for organisation information
    Relates #36

--- src/components/CuratorDetails/CuratorDetails.tsx
+++ src/components/CuratorDetails/CuratorDetails.tsx
@@ -10,8 +10,32 @@ import type { ContentCreator } from "~types/userTypes"
 const CuratorDetails = ({ curator }: { curator: ContentCreator }) => {
   return (
     <div className="content-box shadow stack padding-lg curator-details">
-      <h3 className="content-label">Content Curator</h3>
-      <h2>{curator.curator_name}</h2>
+      <h2 className="content-label text-md">Content Curator</h2>
+      <div className="flex organisation-details">
+        {curator.curator_name && <h3>{curator.curator_name}</h3>}
+        <div className="flex center gap">
+          {curator.curator_organisation && (
+            <h3>{curator.curator_organisation}</h3>
+          )}
+          {curator.organisation_logo && (
+            <img
+              src={
+                process.env.PLASMO_PUBLIC_API_URL +
+                curator.organisation_logo.formats.thumbnail.url
+              }
+              alt={curator.organisation_logo.alternativeText}
+              style={{
+                width:
+                  curator.organisation_logo.formats.thumbnail.width *
+                  0.25,
+                height:
+                  curator.organisation_logo.formats.thumbnail.height *
+                  0.25
+              }}
+            />
+          )}
+        </div>
+      </div>
       <BlocksRenderer content={curator.bio} />
       <div className="margin-top-lg">
         <h3>UP & COMING EVENTS</h3>
