204bcdf5793216341b0e11aaa25626fa018a8aae
Nich Galzin <nichgalzin@gmail.com>
2024-10-08 17:11:02 +0100

    test: adds handler test for new message handler data fetch
    Relates #36

---
+++ src/__tests__/fetchProjectData.js
@@ -0,0 +1,32 @@
+import { beforeEach, describe, expect, it, jest } from "@jest/globals"
+import fetchMock from "jest-fetch-mock"
+
+import handler from "../background/messages/fetchProjectDetailsById"
+import * as testData from "./test-data/current-project-test-data.json"
+
+fetchMock.enableMocks()
+
+describe("fetchProjectById", () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  it("returns a project object to the front from our api", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(testData))
+
+    const req = {
+      name: "fetchCurrentProject"
+    }
+
+    const res = {
+      send: jest.fn()
+    }
+
+    await handler(req, res)
+
+    expect(fetch).toHaveBeenCalled()
+    expect(res.send).toHaveBeenCalledWith(
+      JSON.parse(JSON.stringify(testData))
+    )
+  })
+})
