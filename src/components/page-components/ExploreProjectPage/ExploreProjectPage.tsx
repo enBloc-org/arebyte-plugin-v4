c133ebf76710caa8cf65c832608dcdf349ac1408
Nich Galzin <nichgalzin@gmail.com>
2024-10-08 15:33:27 +0100

    feat: updates useEffect to use message handler for db call
    Relates #36

--- src/components/page-components/ExploreProjectPage/ExploreProjectPage.tsx
+++ src/components/page-components/ExploreProjectPage/ExploreProjectPage.tsx
@@ -1,3 +1,5 @@
+import { sendToBackground } from "@plasmohq/messaging"
+
 import "./ExploreProjectPage.css"
 
 import { useEffect, useState } from "react"
@@ -6,24 +8,21 @@ import BackButton from "~components/BackButton/BackButton"
 import CuratorDetails from "~components/CuratorDetails/CuratorDetails"
 import Footer from "~components/Footer/Footer"
 import ProjectDetails from "~components/ProjectDetails/ProjectDetails"
-import { projectQueryString } from "~queries/projectQuery"
-import type {
-  ProjectData,
-  ProjectResponse
-} from "~types/projectTypes"
-import { fetchStrapiContent } from "~utils/fetchStrapiContent"
+import type { ProjectData } from "~types/projectTypes"
 
 const ExploreProjectPage = () => {
   const [project, setProject] = useState<ProjectData>()
-  console.log(projectQueryString)
 
   useEffect(() => {
     const fetchProject = async () => {
-      console.log(projectQueryString)
+      const response = await sendToBackground({
+        name: "fetchProjectDetailsById",
+        body: {
+          id: 1
+        }
+      })
+      console.log({ response })
 
-      const response = await fetchStrapiContent<ProjectResponse>(
-        `api/projects/1?${projectQueryString}`
-      )
       setProject(response.data as ProjectData)
     }
     fetchProject()
@@ -31,24 +30,28 @@ const ExploreProjectPage = () => {
 
   return (
     <>
-      <main className="explore-project page">
-        <BackButton className="current-project__back-button" />
-        <img
-          src={
-            process.env.PLASMO_PUBLIC_API_URL +
-            project.cover_image.formats.small.url
-          }
-          alt={
-            project.cover_image.alternativeText ||
-            "Project image thumbnail"
-          }
-        />
-        <div className="grid project-details__container">
-          <ProjectDetails project={project} />
-          <CuratorDetails curator={project.content_creator} />
-        </div>
-        <Footer />
-      </main>
+      {project ? (
+        <main className="explore-project page">
+          <BackButton className="explore-project__back-button" />
+          <img
+            src={
+              process.env.PLASMO_PUBLIC_API_URL +
+              project.cover_image.formats.small.url
+            }
+            alt={
+              project.cover_image.alternativeText ||
+              "Project image thumbnail"
+            }
+          />
+          <div className="grid project-details__container">
+            <ProjectDetails project={project} />
+            <CuratorDetails curator={project.content_creator} />
+          </div>
+          <Footer />
+        </main>
+      ) : (
+        <p>Loading</p>
+      )}
     </>
   )
 }
