f491906765226b5f174f4e3613554384525ca8c4
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-10 17:22:04 +0100

    refactor: curry empty changes object to avoid a redundant argument in the recursive function declaration
    relates #48

--- src/utils/updateStorage.ts
+++ src/utils/updateStorage.ts
@@ -1,20 +1,20 @@
 import { UserSession } from "~types/userTypes"
 
+let changes = {}
+
 /**
  *
  * @description recursive function that will check every value of the existing storage against the provided values and update as needed
  * @param currentValue the current state in storage
  * @param newValue any values to be changed in our stored state
- * @param index do not pass anything, it will automatically default to 0
- * @param changes do not pass anything, it will default to an empty object
+ * @param index pass a value only if you intend on skipping the first 'x' number of properties in the store, this argument will automatically default to 0
  * @returns a new full UserSession object to pass into storage with the updated values
  */
 export default function updateStorage(
   currentValue: UserSession,
   newValue: Partial<Omit<UserSession["user"], "audience_member">> &
     Partial<UserSession["user"]["audience_member"]>,
-  index: number = 0,
-  changes = {}
+  index: number = 0
 ) {
   if (index === Object.keys(currentValue).length) {
     return { ...currentValue, ...changes }
@@ -35,8 +35,7 @@ export default function updateStorage(
         [currentKey]: updateStorage(
           currentValue[currentKey],
           newValue,
-          0,
-          {}
+          0
         )
       }
       break
@@ -53,5 +52,5 @@ export default function updateStorage(
       }
   }
 
-  return updateStorage(currentValue, newValue, index + 1, changes)
+  return updateStorage(currentValue, newValue, index + 1)
 }
