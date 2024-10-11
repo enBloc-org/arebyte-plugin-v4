85d59277b514b5becfa1c851c8bf1e4e2714013d
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-11 15:57:43 +0100

    style: animate account settings opening section
    relates #55

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -4,6 +4,8 @@ import useStore from "~store/store"
 
 import "./ProfilePage.css"
 
+import { useState } from "react"
+
 import BurgerMenu from "~components/BurgerMenu/BurgerMenu"
 import ToggleSwitch from "~components/ToggleSwitch/ToggleSwitch"
 import { UserSession } from "~types/userTypes"
@@ -19,6 +21,7 @@ export default function ProfilePage() {
   const [, , { remove }] = useStorage<UserSession>(
     "arebyte-audience-session"
   )
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   const handleQuietSwitchClick = () => {
     updatedIsQuiet(!isQuiet)
@@ -33,43 +36,92 @@ export default function ProfilePage() {
       <BurgerMenu />
       <main className="grid">
         <div className="content-box shadow profile-page--user-details">
-          <h3>{userInfo.username}</h3>
-          <button className="flex center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="0.8em"
-              height="0.8em"
-              viewBox="0 0 32 32"
+          <h3 className="profile-page--user-name">
+            {userInfo.username}
+          </h3>
+
+          <form
+            id="account-settings"
+            className="profile-page--form flex flex-column"
+            aria-hidden={!isOpen}
+          >
+            <input
+              type="text"
+              placeholder={userInfo.username}
+              className="content-box"
+            />
+            <input
+              type="email"
+              placeholder={userInfo.email}
+              className="content-box"
+            />
+            <input
+              type="text"
+              placeholder="Second name"
+              className="content-box"
+            />
+            <input
+              type="date"
+              placeholder="Date of birth"
+              className="content-box"
+            />
+            <input
+              type="text"
+              placeholder="Location (where you live)"
+              className="content-box"
+            />
+            <button type="submit" className="button--primary">
+              submit
+            </button>
+          </form>
+          <div className="profile-page--modal-buttons">
+            <button
+              className={`${isOpen ? "profile-page--arrow-button__open" : "profile-page--arrow-button"}`}
+              aria-controls="account-settings"
+              onClick={() => setIsOpen(previous => !previous)}
             >
-              <path
-                fill="currentColor"
-                d="M3 2.98c0-.569.477-.978 1-.98c.163 0 .33.039.489.125l23.986 13.02c.344.186.525.52.525.855s-.181.67-.525.856L4.49 29.876A1 1 0 0 1 4 30c-.523-.002-1-.411-1-.98z"
-              />
-            </svg>
-            Account Settings
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="0.8em"
+                height="0.8em"
+                viewBox="0 0 32 32"
+                className="profile-page--arrow-icon"
+              >
+                <path
+                  fill="currentColor"
+                  d="M3 2.98c0-.569.477-.978 1-.98c.163 0 .33.039.489.125l23.986 13.02c.344.186.525.52.525.855s-.181.67-.525.856L4.49 29.876A1 1 0 0 1 4 30c-.523-.002-1-.411-1-.98z"
+                />
+              </svg>
+              Account Settings
+            </button>
+
+            {isOpen && (
+              <button
+                type="button"
+                onClick={async () => {
+                  remove()
+                  navigateTo("home")
+                }}
+              >
+                Log me out
+              </button>
+            )}
+          </div>
         </div>
 
         <div className="profile-page--controls flex flex-column start">
-          <ToggleSwitch
-            isChecked={isQuiet}
-            clickHandler={handleQuietSwitchClick}
-          />
+          <div>
+            <ToggleSwitch
+              isChecked={isQuiet}
+              clickHandler={handleQuietSwitchClick}
+            />
+            <p>quiet mode on/off</p>
+          </div>
           <ToggleSwitch
             isChecked={isPaused}
             clickHandler={handlePausedSwitchClick}
           />
         </div>
-
-        <button
-          type="button"
-          onClick={async () => {
-            remove()
-            navigateTo("home")
-          }}
-        >
-          Log Off
-        </button>
       </main>
     </div>
   )
