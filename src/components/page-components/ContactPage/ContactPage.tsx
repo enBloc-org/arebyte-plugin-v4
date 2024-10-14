4171d2c9652badb87671b4539a1c88843ffed0e3
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-14 16:08:51 +0100

    style: arrange and style contact page elements
    relates #44

--- src/components/page-components/ContactPage/ContactPage.tsx
+++ src/components/page-components/ContactPage/ContactPage.tsx
@@ -2,6 +2,8 @@ import BackButton from "~components/BackButton/BackButton"
 
 import "./ContactPage.css"
 
+import Footer from "~components/Footer/Footer"
+
 export default function ContactPage() {
   return (
     <div className="contact-page page background__stripped">
@@ -11,24 +13,27 @@ export default function ContactPage() {
           <input
             type="text"
             placeholder="Name*"
-            className="content-box shadow"
+            className="content-box shadow contact-page--form-input"
           />
           <input
             type="email"
             placeholder="Email*"
-            className="content-box shadow"
+            className="content-box shadow contact-page--form-input"
           />
           <input
             type="text"
             placeholder="Message Title*"
-            className="content-box shadow"
+            className="content-box shadow contact-page--form-input"
           />
-          <input
-            type="text"
+          <textarea
             placeholder="Type message here*"
-            className="content-box shadow"
+            className="content-box shadow contact-page--form-input contact-page--form-input__long"
           />
+          <button className="button--primary bold text-md">
+            send
+          </button>
         </form>
+        <Footer />
       </main>
     </div>
   )
