a991fa54c7471ce3c594b0f872ecbe6e287e8af0
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-15 11:42:41 +0100

    feat: implement new message handler to update the currently active project for this user
    relates #51

--- src/components/SelectProjectButton/SelectProjectButton.tsx
+++ src/components/SelectProjectButton/SelectProjectButton.tsx
@@ -1,19 +1,32 @@
+import { sendToBackground } from "@plasmohq/messaging"
+
 import "./SelectProjectButton.css"
 
 import useStore from "~store/store"
 
-export default function SelectProjectButton(projectId: number) {
+export default function SelectProjectButton() {
   const {
     audience_member: { project_id: currentProjectId }
   } = useStore.use.user()
   const isLoggedIn = useStore.use.isLoggedIn()
 
+  const exploreProjectId = useStore.use.exploreProjectId()
+
+  const handleClick = async () => {
+    console.log(exploreProjectId)
+    await sendToBackground({
+      name: "selectNewActiveProject",
+      body: { selectedProjectId: exploreProjectId }
+    })
+  }
+
   return (
     <button
       className="button--primary shadow text-md select-project-button"
-      disabled={projectId === currentProjectId || !isLoggedIn}
+      disabled={exploreProjectId === currentProjectId || !isLoggedIn}
+      onClick={handleClick}
     >
-      {projectId === currentProjectId
+      {exploreProjectId === currentProjectId
         ? "this is your current project"
         : "make this current project"}
     </button>
