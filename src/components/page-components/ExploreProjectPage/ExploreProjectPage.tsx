ec5d7dc161c9294057057924caa783db98fe99de
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-16 12:52:40 +0100

    fix:  remove deprecated type import
    relates #51

--- src/components/page-components/ExploreProjectPage/ExploreProjectPage.tsx
+++ src/components/page-components/ExploreProjectPage/ExploreProjectPage.tsx
@@ -2,7 +2,7 @@ import { sendToBackground } from "@plasmohq/messaging"
 
 import "./ExploreProjectPage.css"
 
-import { useEffect, useState } from "react"
+import { ErrorInfo, useEffect, useState } from "react"
 import { useErrorBoundary } from "react-error-boundary"
 
 import BackButton from "~components/BackButton/BackButton"
@@ -11,10 +11,7 @@ import Footer from "~components/Footer/Footer"
 import ProjectDetails from "~components/ProjectDetails/ProjectDetails"
 import SelectProjectButton from "~components/SelectProjectButton/SelectProjectButton"
 import useStore from "~store/store"
-import type {
-  ProjectData,
-  ProjectResponse
-} from "~types/projectTypes"
+import type { ProjectData } from "~types/projectTypes"
 
 const ExploreProjectPage = () => {
   const [project, setProject] = useState<ProjectData>()
@@ -23,14 +20,13 @@ const ExploreProjectPage = () => {
 
   useEffect(() => {
     const fetchProject = async () => {
-      const { data, error }: ProjectResponse = await sendToBackground(
-        {
+      const { data, error }: { data: ProjectData; error: ErrorInfo } =
+        await sendToBackground({
           name: "fetchProjectDetailsById",
           body: {
             id: exploreProjectId
           }
-        }
-      )
+        })
       if (error) showBoundary(error)
       setProject(data)
     }
