bc6ad1cdcebbe076f3418d4658bf9950df1b1528
Nich Galzin <nichgalzin@gmail.com>
2024-10-16 16:14:15 +0100

    feat: message handler for creating a new user
    Relates #61

---
+++ src/background/messages/createNewUser.ts
@@ -0,0 +1,16 @@
+import type { PlasmoMessaging } from "@plasmohq/messaging"
+
+import type { AuthResponse } from "~types/userTypes"
+import { fetchStrapiContent } from "~utils/fetchStrapiContent"
+
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+  const data = await fetchStrapiContent<AuthResponse>(
+    `api/auth/local/register`,
+    "POST",
+    undefined,
+    req.body
+  )
+  res.send(data)
+}
+
+export default handler
