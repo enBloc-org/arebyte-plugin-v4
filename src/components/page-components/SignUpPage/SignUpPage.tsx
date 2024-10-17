f522cb46eee7409b147fe8695b6837f54cef8ae7
Nich Galzin <nichgalzin@gmail.com>
2024-10-17 14:15:04 +0100

    refactor: adds validation to match strappi backend limitations
    Relates #61

--- src/components/page-components/SignUpPage/SignUpPage.tsx
+++ src/components/page-components/SignUpPage/SignUpPage.tsx
@@ -37,9 +37,11 @@ const SignUpPage = () => {
           initialValues={initialValues}
           validationSchema={Yup.object({
             username: Yup.string()
+              .min(3, "Must be longer then 3 characters")
               .max(15, "Must be 15 characters or less")
               .required("Required"),
             email: Yup.string()
+              .min(6, "Must be longer then 6 characters")
               .email("Invalid email address")
               .required("Required"),
             password: Yup.string()
