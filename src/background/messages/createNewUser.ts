83662024fa1ab1348bf9d33ba7ac60436a8dc35c
Nich Galzin <nichgalzin@gmail.com>
2024-10-17 17:28:18 +0100

    refactor: implents logic for authentication flow with the new data structure
    Relates #61

--- src/background/messages/createNewUser.ts
+++ src/background/messages/createNewUser.ts
@@ -1,33 +1,9 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-import { AudienceMemberQueryString } from "~queries/audienceMemberQuery"
-import type {
-  AudienceMember,
-  AuthData,
-  UserSession
-} from "~types/userTypes"
+import type { AuthData } from "~types/userTypes"
 import { fetchStrapiContent } from "~utils/fetchStrapiContent"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  // Create new audience member and git id to connect to new user
-  const { data: audienceData, error: audienceError } =
-    await fetchStrapiContent<AudienceMember>(
-      `api/audience-members?${AudienceMemberQueryString}`,
-      "POST",
-      undefined,
-      JSON.stringify({
-        project_id: 0,
-        is_quiet_mode: false,
-        is_paused: false,
-        event_time: "12:00:00:000"
-      })
-    )
-
-  if (audienceError) {
-    console.log(audienceError)
-    res.send(false)
-  }
-
   // Register new user and add audience member relation
   const response = await fetchStrapiContent<AuthData>(
     `api/auth/local/register`,
@@ -35,34 +11,16 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
     undefined,
     JSON.stringify({
       ...req.body,
-      audience_member: audienceData.id
+      event_time: "12:00:00.000"
     })
   )
 
   if (response.error) {
     console.log(response.error)
-    res.send(response.error)
-  }
-  interface UserSessionWithError extends UserSession {
-    error: string
-  }
-  const userSession: UserSessionWithError = {
-    user: {
-      ...response.data.user,
-      audience_member: {
-        is_quiet: audienceData.is_quiet,
-        is_paused: audienceData.is_quiet,
-        project_id: audienceData.project_id,
-        current_index: audienceData.current_index,
-        event_time: audienceData.event_time,
-        playlist: []
-      }
-    },
-    jwt: response.data.jwt,
-    error: response.error
+    res.send(response)
   }
 
-  res.send(userSession)
+  res.send(response)
 }
 
 export default handler
