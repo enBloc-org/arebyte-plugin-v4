20534b4b918b1af9caa27a3751e00247a155acd0
Nich Galzin <nichgalzin@gmail.com>
2024-10-21 17:38:02 +0100

    fix: fixes edge cases where different size browser screens effect the popup window sizess
    Relates #40

--- src/utils/backgroundPopCreate.ts
+++ src/utils/backgroundPopCreate.ts
@@ -4,11 +4,17 @@ import calculatePopupCoordinates from "./calculatePopupCoordinates"
 import parseImageSize from "./parseImageSize"
 
 const backgroundPopupCreate = async popups => {
-  const currentWindow = await Browser.windows.getCurrent()
-  const screenHeight = currentWindow.height
-  const screenWidth = currentWindow.width
+  const screenDimensions = await Browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(tabs =>
+      Browser.tabs.sendMessage(tabs[0].id, {
+        action: "getScreenDimensions"
+      })
+    )
+  const { width: screenWidth, height: screenHeight } =
+    screenDimensions as { width: number; height: number }
 
-  popups.forEach((popup, index) => {
+  popups.forEach(async popup => {
     const { height, width, url } = parseImageSize(popup)
 
     const { top, left } = calculatePopupCoordinates(
@@ -19,16 +25,25 @@ const backgroundPopupCreate = async popups => {
       height
     )
 
-    setTimeout(() => {
-      Browser.windows.create({
-        url: `/tabs/newTab.html?url=${url}&width=${width}&height=${height}`,
-        type: "popup",
-        width: width,
-        height: height,
-        top: Math.floor(top),
-        left: Math.floor(left)
+    const popupWindow = await Browser.windows.create({
+      url: `/tabs/newTab.html?url=${url}&width=${width}&height=${height}`,
+      type: "popup",
+      width: width,
+      height: height,
+      state: "normal",
+      top: Math.floor(top),
+      left: Math.floor(left)
+    })
+
+    if (
+      popupWindow.width !== width ||
+      popupWindow.height !== height
+    ) {
+      await Browser.windows.update(popupWindow.id, {
+        width: Math.floor(width),
+        height: Math.floor(height)
       })
-    }, index * 1000)
+    }
   })
 }
 
