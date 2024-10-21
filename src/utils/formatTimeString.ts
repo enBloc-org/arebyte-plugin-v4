a39d8ce9961c8358aa45ccd82ce2fb2465042caf
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-21 16:27:03 +0100

    docs: documented return type and use case for new util
    relates #54

--- src/utils/formatTimeString.ts
+++ src/utils/formatTimeString.ts
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param timeString expects the time string retrieved from our data types in the event_time property
+ * @returns a string formatted to the appropriate shape for our CMS "00:00:00.000"
+ */
 export default function formatTimeString(timeString: string) {
   const [hourDigits, minuteDigits] = timeString.split(":")
 
