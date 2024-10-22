fc1457a91219141c8eff1810d0f1e4bb2b40972c
Nich Galzin <nichgalzin@gmail.com>
2024-10-22 18:24:27 +0100

    feat: first draft popup component that renders different content types
    Support video, image and text
    Relates #40

--- src/tabs/newTab.tsx
+++ src/tabs/newTab.tsx
@@ -1,16 +1,67 @@
+import { useEffect, useState } from "react"
+
+import "./NewTab.css"
+
+import { BlocksRenderer } from "@strapi/blocks-react-renderer"
+import Browser from "webextension-polyfill"
+
+import { Popup, SlimPopup } from "~types/eventTypes"
+
 const NewTab = () => {
   const params = new URLSearchParams(window.location.search)
   const width = params.get("width")
   const height = params.get("height")
-  const url = "http://localhost:1337" + params.get("url")
-
+  const url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:1337" + params.get("url")
+      : params.get("url")
+  const index = params.get("index")
+  const [popup, setPopup] = useState<SlimPopup>()
+  useEffect(() => {
+    const getFromStorage = async () => {
+      const { popups } = await Browser.storage.session.get(["popups"])
+      console.log("====================================")
+      console.log(popups)
+      console.log("====================================")
+      setPopup(popups[index])
+    }
+    getFromStorage()
+  }, [])
   return (
-    <div>
-      <img
+    <div style={{ width: width, height: height }}>
+      {popup && (
+        <>
+          {popup.type === "text" && (
+            <>
+              <h1>{popup.popupInfo.work_title}</h1>
+              <BlocksRenderer content={popup.text_content} />
+              <h1>{popup.popupInfo.work_title}</h1>
+              <BlocksRenderer content={popup.text_content} />
+            </>
+          )}
+          {popup.type === "image" && (
+            <img
+              src={url}
+              alt="an image"
+              style={{ width: width, height: height }}
+            />
+          )}
+          {popup.type === "video" && (
+            <video
+              src={url}
+              autoPlay
+              controls
+              style={{ width: width, height: height }}
+            />
+          )}
+        </>
+      )}
+
+      {/* <img
         src={url}
         alt="an image"
-        style={{ width: Number(width), height: Number(height) }}
-      />
+        style={{ width: width, height: height }}
+      /> */}
     </div>
   )
 }
