dba4f22aff241e1f405345030885bcaead3e2349
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-23 11:41:36 +0100

    refactor: await alarm operations to return promise outcome
    relates #71

--- src/utils/setEventAlarm.ts
+++ src/utils/setEventAlarm.ts
@@ -10,19 +10,24 @@ import eventAlarmListener from "./eventAlarmListener"
  * @param eventMinute
  * @description sets a unique alarm for our daily event. This function should be called every time the event_time value is updated to align the extensions behaviour with the state of our data.
  */
-export default function setEventAlarm(
+export default async function setEventAlarm(
   eventHour: number,
   eventMinute: number
 ) {
-  browser.alarms.clear("sequence-alarm")
-  browser.alarms.onAlarm.removeListener(eventAlarmListener)
+  await browser.alarms.clear("sequence-alarm")
+  await browser.alarms.onAlarm.removeListener(eventAlarmListener)
 
-  browser.alarms.create("sequence-alarm", {
+  await browser.alarms.create("sequence-alarm", {
     periodInMinutes: 1440,
     when: calculateCountDown(eventHour, eventMinute)
   })
-  browser.alarms.onAlarm.addListener(eventAlarmListener)
+  await browser.alarms.onAlarm.addListener(eventAlarmListener)
+  try {
+    const newAlarm = await browser.alarms.get("sequence-alarm")
 
-  const newAlarm = browser.alarms.get("sequence-alarm")
-  return newAlarm
+    return { data: newAlarm, error: null }
+  } catch (error) {
+    console.error(error)
+    return { data: null, error: error }
+  }
 }
