61d398b64450f12d8455510f498fc228adf5d681
Nich Galzin <nichgalzin@gmail.com>
2024-10-23 12:54:02 +0100

    fix: updates arguments to deal with type issues
    Relates #40

--- src/utils/popupUtils/parseImageSize.ts
+++ src/utils/popupUtils/parseImageSize.ts
@@ -1,21 +1,24 @@
-import type { Popup } from "~types/eventTypes"
+import type { MediaContent } from "~types/eventTypes"
 
 /**
  * @description Parse the size of an image
  * @param {Popup} popup - the popup object
+ *@param {size} size - the size of the popup
+ * @param {popupContent} popupContent - the content of the popup
  * @returns {object} - the width, height, and url of the image
  */
 
 const parseImageSize = (
-  popup: Popup
+  size: string,
+  popupContent: MediaContent
 ): { width: number; height: number; url: string } => {
-  const size = popup.popup_size.toLowerCase().trim()
-  const format = popup.popup_content[0].media.formats[size]
+  const sizeStr = size.toLowerCase().trim()
+  const format = popupContent[0].media.formats[sizeStr]
 
   const imageSize = {
-    height: format?.height ?? popup.popup_content[0].media.height,
-    width: format?.width ?? popup.popup_content[0].media.width,
-    url: format?.url ?? popup.popup_content[0].media.url
+    height: format?.height ?? popupContent[0].media.height,
+    width: format?.width ?? popupContent[0].media.width,
+    url: format?.url ?? popupContent[0].media.url
   }
 
   return imageSize
