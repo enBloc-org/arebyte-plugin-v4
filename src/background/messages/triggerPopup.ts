e0670075c4332e60605552c5f42771944065664a
Nich Galzin <nichgalzin@gmail.com>
2024-10-23 12:56:14 +0100

    fix: fixes import statment and adjust function call to pass correct argument
    Relates #40

--- src/background/messages/triggerPopup.ts
+++ src/background/messages/triggerPopup.ts
@@ -2,14 +2,14 @@ import type { PlasmoMessaging } from "@plasmohq/messaging"
 
 import { eventPopupQueryString } from "~queries/eventPopupsQuery"
 import type { EventData } from "~types/eventTypes"
-import backgroundPopupCreate from "~utils/backgroundPopCreate"
 import { fetchStrapiContent } from "~utils/fetchStrapiContent"
+import backgroundPopupCreate from "~utils/popupUtils/backgroundPopCreate"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   const { data } = await fetchStrapiContent<EventData>(
     `api/events/${req.body.id}?${eventPopupQueryString}`
   )
-  backgroundPopupCreate(data)
+  backgroundPopupCreate(data.pop_ups)
   res.send("success!")
 }
 
