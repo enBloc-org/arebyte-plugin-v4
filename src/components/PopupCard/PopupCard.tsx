ac7e0311cbffa31e483ec34a6d4059edddcde81e
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-24 15:20:31 +0100

    feat: implement event handler to trigger new message handler on click for PopupCard component
    relates #64

--- src/components/PopupCard/PopupCard.tsx
+++ src/components/PopupCard/PopupCard.tsx
@@ -2,6 +2,8 @@ import type { Popup } from "~types/eventTypes"
 
 import "./PopupCard.css"
 
+import { sendToBackground } from "@plasmohq/messaging"
+
 export default function PopupCard({
   popup,
   isEditing
@@ -9,17 +11,30 @@ export default function PopupCard({
   popup: Popup
   isEditing?: boolean
 }) {
+  const handleClick = async () => {
+    const { data, error } = await sendToBackground({
+      name: "viewSinglePopup",
+      body: { id: popup.id }
+    })
+
+    if (error) console.log("ooops!")
+
+    console.log(data)
+  }
+
   return (
-    <div className="popup-card content-box shadow">
-      <img
-        className="popup-card--image"
-        src={
-          process.env.PLASMO_PUBLIC_API_URL +
-          popup.thumbnail_image.formats.thumbnail.url
-        }
-        alt={popup.work_title}
-      />
-      {isEditing && <span>REMOVE</span>}
-    </div>
+    <button onClick={handleClick}>
+      <div className="popup-card content-box shadow">
+        <img
+          className="popup-card--image"
+          src={
+            process.env.PLASMO_PUBLIC_API_URL +
+            popup.thumbnail_image.formats.thumbnail.url
+          }
+          alt={popup.work_title}
+        />
+        {isEditing && <span>REMOVE</span>}
+      </div>
+    </button>
   )
 }
