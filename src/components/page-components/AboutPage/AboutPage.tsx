cda839ef3ef52e677aa25f90f95556c8bc5e34e2
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-10-30 10:50:00 +0000

    feat: render cms content to about page
    relates #45

--- src/components/page-components/AboutPage/AboutPage.tsx
+++ src/components/page-components/AboutPage/AboutPage.tsx
@@ -1,12 +1,18 @@
+import { BlocksRenderer } from "@strapi/blocks-react-renderer"
 import { useEffect, useState } from "react"
 import { useErrorBoundary } from "react-error-boundary"
 
+import "./AboutPage.css"
+
 import { sendToBackground } from "@plasmohq/messaging"
 
+import BackButton from "~components/BackButton/BackButton"
 import type { AboutPage } from "~types/baseTypes"
 
 export default function AboutPage() {
-  const [content, setContent] = useState("")
+  const [content, setContent] = useState<AboutPage | undefined>(
+    undefined
+  )
   const { showBoundary } = useErrorBoundary()
 
   useEffect(() => {
@@ -21,16 +27,39 @@ export default function AboutPage() {
 
       if (error) return showBoundary(error)
 
-      setContent(data.abstract)
+      setContent(data)
     }
 
     getContent()
   }, [])
 
   return (
-    <>
-      <h1>ABOUT</h1>
-      {content && <p>{content}</p>}
-    </>
+    <div className="about-page page">
+      <main className="grid">
+        <BackButton />
+        {content && (
+          <div className="about-page--content content-box shadow stack">
+            <section>
+              <p className="bold text-md">About</p>
+              <p className="bold text-md">{content.abstract}</p>
+            </section>
+            <section>
+              <BlocksRenderer content={content.description} />
+            </section>
+          </div>
+        )}
+        <div className="about-page--credits content-box shadow stack">
+          <p className="bold">Credits</p>
+          <p className="bold">
+            Powered by <a href="https://www.arebyte.com/">arebyte</a>,
+            2024
+          </p>
+          <p className="bold">
+            Plugin Design and Development by enBloc
+          </p>
+          <p className="bold">all rights reserved to arebyte 2024</p>
+        </div>
+      </main>
+    </div>
   )
 }
