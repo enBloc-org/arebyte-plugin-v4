f700988f835671b190c22d1f60520202b82afda4
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-11-06 10:59:16 +0000

    feat: populate new DigestPage with the titles of each sequence in a project
    relates #109

--- src/components/page-components/DigestPage/DigestPage.tsx
+++ src/components/page-components/DigestPage/DigestPage.tsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react"
+import { useErrorBoundary } from "react-error-boundary"
+
+import { sendToBackground } from "@plasmohq/messaging"
+
+import BurgerMenu from "~components/BurgerMenu/BurgerMenu"
+import Footer from "~components/Footer/Footer"
+import type { EventData } from "~types/eventTypes"
+import { ProjectData } from "~types/projectTypes"
+
 export default function DigestPage() {
-  return <h1>Digest</h1>
+  const [events, setEvents] = useState<
+    Omit<EventData, "pop_ups">[] | null
+  >(null)
+  const { showBoundary } = useErrorBoundary()
+
+  useEffect(() => {
+    const getEvents = async () => {
+      const {
+        data,
+        error
+      }: { data: ProjectData; error: string | null } =
+        await sendToBackground({
+          name: "fetchListForDigest"
+        })
+
+      if (error) showBoundary(error)
+
+      setEvents(data.sequence)
+    }
+
+    getEvents()
+  }, [])
+
+  return (
+    <div className="digest-page page">
+      <BurgerMenu />
+      <main className="grid">
+        {events &&
+          events.map(event => (
+            <div key={event.id}>{event.title}</div>
+          ))}
+      </main>
+      <Footer />
+    </div>
+  )
 }
