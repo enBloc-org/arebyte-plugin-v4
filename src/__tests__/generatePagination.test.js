34c3175ffd5db5453466ea33c5c326b3e0a7beeb
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-11-07 16:06:03 +0000

    test: test return value of generatePagination
    relates #109

---
+++ src/__tests__/generatePagination.test.js
@@ -0,0 +1,41 @@
+import { describe, expect, it } from "@jest/globals"
+
+import generatePagination from "../utils/generatePagination"
+
+describe("generatePagination", () => {
+  it("returns the correct page number", () => {
+    const { pageNumber: result1 } = generatePagination(4)
+    const { pageNumber: result2 } = generatePagination(8)
+    const { pageNumber: result3 } = generatePagination(19)
+    const { pageNumber: result4 } = generatePagination(22)
+
+    expect(result1).toBe(1)
+    expect(result2).toBe(2)
+    expect(result3).toBe(3)
+    expect(result4).toBe(4)
+  })
+
+  describe("returns the correct page size", () => {
+    const { pageSize: result1 } = generatePagination(4)
+    const { pageSize: result2 } = generatePagination(8)
+    const { pageSize: result3 } = generatePagination(19)
+    const { pageSize: result4 } = generatePagination(22)
+
+    expect(result1).toBe(4)
+    expect(result2).toBe(1)
+    expect(result3).toBe(5)
+    expect(result4).toBe(1)
+  })
+
+  describe("returns a full page if the given number is multiple of 7", () => {
+    const { pageNumber: number1, pageSize: size1 } =
+      generatePagination(7)
+    const { pageNumber: number2, pageSize: size2 } =
+      generatePagination(14)
+
+    expect(number1).toBe(1)
+    expect(size1).toBe(7)
+    expect(number2).toBe(2)
+    expect(size2).toBe(7)
+  })
+})
