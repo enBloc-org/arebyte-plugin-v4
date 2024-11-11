e0d8fab2914d9a4cb04df026147e2b65924de5e4
Nich Galzin <nichgalzin@gmail.com>
2024-11-11 14:46:39 +0000

    feat: page and form for password check
    Relates #100

---
+++ src/components/page-components/PasswordResetPage/PasswordResetPage.tsx
@@ -0,0 +1,124 @@
+import { Form, Formik } from "formik"
+import { useState } from "react"
+import * as Yup from "yup"
+
+import { sendToBackground } from "@plasmohq/messaging"
+
+import BackButton from "~components/BackButton/BackButton"
+import Footer from "~components/Footer/Footer"
+import PasswordInput from "~components/Forms/PasswordInput/PasswordInput/PasswordInput"
+
+import "./PasswordResetPage.css"
+
+import useStore from "~store/store"
+import { AuthData } from "~types/userTypes"
+
+const initialValues = {
+  currentPassword: "",
+  password: "",
+  passwordCheck: ""
+}
+const PasswordResetPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+  const navigateTo = useStore.use.navigateTo()
+
+  return (
+    <div className="password-reset-page page background__stripped">
+      <BackButton />
+      <main>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={Yup.object({
+            currentPassword: Yup.string().min(
+              8,
+              "Password must be at least 8 characters"
+            ),
+            password: Yup.string()
+              .min(8, "Password must be at least 8 characters")
+              .matches(
+                /[a-z]/,
+                "Password must contain at least one lowercase letter"
+              )
+              .matches(
+                /[A-Z]/,
+                "Password must contain at least one uppercase letter"
+              )
+              .matches(
+                /[0-9]/,
+                "Password must contain at least one number"
+              )
+              .matches(
+                /[^a-zA-Z0-9]/,
+                "Password must contain at least one special character"
+              )
+              .required("Password is required"),
+            passwordCheck: Yup.string()
+              .oneOf([Yup.ref("password")], "Passwords must match")
+              .required("Please confirm your password")
+          })}
+          onSubmit={async (values, actions) => {
+            setIsLoading(true)
+            setErrorMessage("")
+            const {
+              data,
+              error
+            }: { data: AuthData; error: string | null } =
+              await sendToBackground({
+                name: "resetUserPassword",
+                body: {
+                  currentPassword: values.currentPassword,
+                  password: values.password,
+                  passwordCheck: values.passwordCheck
+                }
+              })
+            if (error) {
+              setErrorMessage(error)
+              setIsLoading(false)
+              return actions.setSubmitting(false)
+            }
+            if (data) {
+              actions.setSubmitting(false)
+              setIsLoading(false)
+              navigateTo("profile")
+            }
+          }}
+        >
+          <Form className="form--container stack">
+            <legend className="bold">RESET PASSWORD</legend>
+            <PasswordInput
+              name="currentPassword"
+              placeholder="Current Password*"
+            />
+            <PasswordInput name="password" placeholder="Password*" />
+            <PasswordInput
+              name="passwordCheck"
+              placeholder="Re-enter password*"
+            />
+
+            <div className="flex sign-up--buttons">
+              <button
+                type="submit"
+                className="button--primary text-md"
+                disabled={isLoading}
+              >
+                Submit
+              </button>
+            </div>
+          </Form>
+        </Formik>
+        {isLoading && (
+          <p className="message margin-top-sm text-lg">Loading</p>
+        )}
+        {errorMessage && (
+          <p className="message__error margin-top-sm text-lg">
+            {errorMessage}
+          </p>
+        )}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default PasswordResetPage
