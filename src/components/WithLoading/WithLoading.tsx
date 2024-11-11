c4abd5b626c6a079489a8110919cbecc4c1e56c8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-11-11 09:05:16 +0000

    docs: record use case and prop types explicitly for new component
    relates #112/loading-favourites

--- src/components/WithLoading/WithLoading.tsx
+++ src/components/WithLoading/WithLoading.tsx
@@ -2,7 +2,17 @@ import logo from "data-base64:assets/arebyte-Plugin-blue.png"
 
 import "./WithLoading.css"
 
-export default function WithLoading({ children, isLoading }) {
+/**
+ *
+ * @returns a new component that wraps the children with a loading state dynamically controlled by the isLoading boolean
+ */
+export default function WithLoading({
+  children,
+  isLoading
+}: {
+  children: JSX.Element
+  isLoading: boolean
+}) {
   if (isLoading)
     return <img className="with-loading__loading" src={logo} />
 
