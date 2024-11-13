abe29ebc591ec20b7e258b90ea2e11ae63ec57c5
Nich Galzin <nichgalzin@gmail.com>
2024-11-13 11:19:38 +0000

    refactor: fetches favourite ids to check for button state
    Relates #133

--- src/components/popup-components/ManageFavouritesButton/ManageFavouritesButton.tsx
+++ src/components/popup-components/ManageFavouritesButton/ManageFavouritesButton.tsx
@@ -1,12 +1,46 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { sendToBackground } from "@plasmohq/messaging"
 
+import { UserFavourites, UserSession } from "~types/userTypes"
+import newStorage from "~utils/newStorage"
+
 const ManageFavouritesButton = ({ popupId }) => {
+  const storage = newStorage()
   const [status, setStatus] = useState<
     "idle" | "loading" | "rejected"
   >("idle")
   const [isFavourite, setIsFavourite] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetchFavourites = async () => {
+      const userSession: UserSession = await storage.get(
+        "arebyte-audience-session"
+      )
+      const {
+        data,
+        error
+      }: { data: UserFavourites; error: string | null } =
+        await sendToBackground({
+          name: "fetchUserFavourites",
+          body: {
+            id: userSession.id,
+            jwt: userSession.jwt
+          }
+        })
+
+      if (error) {
+        console.error(error)
+        return
+      }
+      console.log(data)
+      console.log(popupId)
+      console.log(data.favourites.some(fav => fav.id === popupId))
+      setIsFavourite(data.favourites.some(fav => fav.id === popupId))
+    }
+
+    fetchFavourites()
+  }, [])
 
   const clickHandler = async () => {
     setStatus("loading")
