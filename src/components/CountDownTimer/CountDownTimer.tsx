e0d1ed242524570aee033ca8714d8c92014bacc0
Nich Galzin <nichgalzin@gmail.com>
2024-11-26 10:26:14 -0600

    refactor: adss close all popups to button to countdown timer
    Relates #142

--- src/components/CountDownTimer/CountDownTimer.tsx
+++ src/components/CountDownTimer/CountDownTimer.tsx
@@ -5,6 +5,7 @@ import useStore from "~store/store"
 import "./CountDownTimer.css"
 
 import PauseSwitch from "~components/PauseSwitch/PauseSwitch"
+import CloseAllPopupsButton from "~components/popup-components/CloseAllPopupsButton/CloseAllPopupButton"
 
 export default function CountDownTimer() {
   const [tHours, setTHours] = useState(0)
@@ -62,7 +63,10 @@ export default function CountDownTimer() {
           </h2>
         )}
       </div>
-      <PauseSwitch showlabel={false} className="timer__pause-switch" />
+      <div className="flex timer__controls">
+        <PauseSwitch showlabel={false} />
+        <CloseAllPopupsButton />
+      </div>
     </div>
   )
 }
